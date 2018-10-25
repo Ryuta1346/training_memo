6ab6d447cdbf7acbfbc3686befa4e7fc0a9e4714
Ryu007 <ryuta.tashiro@gmail.com>
2018-10-25 22:59:37 +0900

    Modify home.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,30 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def new
+  end
+
+  def create
+    @post = current_user.posts.build(post_parameter)
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to root_url
+    else
+      flash[:danger] = "Post Not created!"
+      redirect_to root_url
+    end
+  end
+
+
+  private
+
+  def post_parameter
+    params.require(:post).permit(:content, :weight, :body_fat_percentage, :date)
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if user_signed_in?
+      @post  = current_user.posts.build
+      @posts = current_user.posts.order(date: "DESC")
+    end
   end
 
   def about
