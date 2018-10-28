679ab9b1167a958c499b98863e05230b5f26fb51
Ryu007 <ryuta.tashiro@gmail.com>
2018-10-29 01:18:14 +0900

    Modify controller, post.rb and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post  = Post.find(params[:id])
+    @posts = Post.all
   end
 
   def edit
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(date: :desc)}
   validates :user_id, presence: true
-  validates :content, presence: true
+  validates :training, presence: true
+  validates :weight,  presence:true
   validates :date, uniqueness: true
-
-
 end
