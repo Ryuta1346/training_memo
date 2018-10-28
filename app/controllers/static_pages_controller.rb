5ee8cb50f0573a76750785642cd97cf84dbce078
Ryu007 <ryuta.tashiro@gmail.com>
2018-10-28 21:32:32 +0900

    Modify views and static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
-      @post  = current_user.posts.build
-      @posts = current_user.posts.all
-      @recent_weight = current_user.posts.first.weight || current_user.weight
-      @recent_bmi = ((current_user.posts.first.weight).to_f/(current_user.height).to_f/(current_user.height).to_f).round(2)
+      @post           = current_user.posts.build
+      @posts          = current_user.posts.all
+      @recent_weight  = current_user.posts.first.weight || current_user.weight
+      @recent_bmi     = ((current_user.posts.first.weight).to_f / (current_user.height).to_f / (current_user.height).to_f).round(2)
+      @by_achievement = ((@recent_weight if @posts.any?) - (current_user.aim)).round(2)
+      @by_last        = ((Date.today - current_user.started_at) + 1).to_i
     end
   end
 
