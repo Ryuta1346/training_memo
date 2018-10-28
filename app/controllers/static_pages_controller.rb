78f62842ca284def71d0fd8dd96d2c396a0adc87
Ryu007 <ryuta.tashiro@gmail.com>
2018-10-28 21:54:24 +0900

    Modify user_top.html.erb and static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,9 @@ class StaticPagesController < ApplicationController
     if user_signed_in?
       @post           = current_user.posts.build
       @posts          = current_user.posts.all
-      @recent_weight  = current_user.posts.first.weight || current_user.weight
-      @recent_bmi     = ((current_user.posts.first.weight).to_f / (current_user.height).to_f / (current_user.height).to_f).round(2)
+      @first_bmi      = (current_user.weight / current_user.height / current_user.height).round(2)
+      @recent_weight  = @posts.any? ? current_user.posts.first.weight : current_user.weight
+      @recent_bmi     = @posts.any? ? ((current_user.posts.first.weight).to_f / (current_user.height).to_f / (current_user.height).to_f).round(2) : @first_bmi
       @by_achievement = ((@posts.any? ? @recent_weight : current_user.weight).to_f - (current_user.aim).to_f).round(2)
       @by_last        = (((Date.today) - current_user.started_at) + 1).to_i
     end
