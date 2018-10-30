465cade9e25908afcff4370125bbcbf12ec021cb
Ryu007 <ryuta.tashiro@gmail.com>
2018-10-31 02:53:46 +0900

    Modify index and show views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users          = User.all
+    @posts          = Post.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user           = User.find(params[:id])
+    @posts          = @user.posts.all
+    @first_bmi      = (@user.weight / @user.height / @user.height).round(2)
+    @recent_weight  = @posts.any? ? @user.posts.first.weight : @user.weight
+    @recent_bmi     = @posts.any? ? ((@user.posts.first.weight).to_f / (@user.height).to_f / (@user.height).to_f).round(2) : @first_bmi
+    @by_achievement = ((@posts.any? ? @recent_weight : @user.weight).to_f - (@user.aim).to_f).round(2)
+    @by_last        = ((Date.today) - @user.started_at + 1).to_i
   end
 end
