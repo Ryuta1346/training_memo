d00c3a9ca8454760dae7d5dc5e1d29ad253f436c
Ryu007 <ryuta.tashiro@gmail.com>
2018-11-02 22:08:26 +0900

    Modify carrier_wave.rb and production.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,12 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
+        # Amazon S3用の設定
         :provider              => 'AWS',
-        :region                => ENV.fetch(:S3_REGION),
-        :aws_access_key_id     => ENV.fetch(:S3_ACCESS_KEY),
-        :aws_secret_access_key => ENV.fetch(:S3_SECRET_KEY)
+        :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory    =  ENV.fetch(:S3_BUCKET)
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
