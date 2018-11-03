920e6a4ea343be38bf9a3ce2782d92328ccb7e02
Ryu007 <ryuta.tashiro@gmail.com>
2018-11-03 23:33:18 +0900

    Modify views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def full_title(page_title="")
+    base_title = "100Days Of Training"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
   def current_user?(user)
     user == current_user
   end
