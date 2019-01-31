34136919e09531c3b3335a306394e00ec507484f
Ryu007 <ryuta.tashiro@gmail.com>
2019-02-01 08:05:45 +0900

    Modify controllers for BMI

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,9 @@ class StaticPagesController < ApplicationController
       user            = current_user
       @post           = user.posts.build
       @posts          = user.posts.all
-      @first_bmi      = (user.weight / user.height / user.height).round(2)
+      @first_bmi      = (user.weight / (user.height ** 2)).round(2)
       @recent_weight  = @posts.any? ? user.posts.first.weight : user.weight
-      @recent_bmi     = @posts.any? ? (user.posts.first.weight / user.height / user.height).round(2) : @first_bmi
+      @recent_bmi     = @posts.any? ? (user.posts.first.weight / (user.height ** 2)).round(2) : @first_bmi
       @by_achievement = ((@posts.any? ? @recent_weight : user.weight) - user.aim).round(2)
       @by_last        = (Date.today - user.started_at + 1).to_i
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
     @user           = User.find(params[:id])
     user            = @user
     @posts          = user.posts.all
-    @first_bmi      = (user.weight / user.height / user.height).round(2)
+    @first_bmi      = (user.weight / (user.height ** 2)).round(2)
     @recent_weight  = @posts.any? ? user.posts.first.weight : user.weight
-    @recent_bmi     = @posts.any? ? (user.posts.first.weight / user.height / user.height).round(2) : @first_bmi
+    @recent_bmi     = @posts.any? ? (user.posts.first.weight / (user.height ** 2)).round(2) : @first_bmi
     @by_achievement = ((@posts.any? ? @recent_weight : user.weight) - user.aim).round(2)
   end
 end
