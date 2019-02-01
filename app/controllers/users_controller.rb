b745c369def7e3690c6d2c7543a86a6a484d4d38
Ryu007 <ryuta.tashiro@gmail.com>
2019-02-01 20:28:54 +0900

    Modify users_controller.rb and user.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user           = User.find(params[:id])
-    user            = @user
-    @posts          = user.posts.all
-    @first_bmi      = (user.weight / (user.height ** 2)).round(2)
-    @recent_weight  = @posts.any? ? user.posts.first.weight : user.weight
-    @recent_bmi     = @posts.any? ? (user.posts.first.weight / (user.height ** 2)).round(2) : @first_bmi
-    @by_achievement = ((@posts.any? ? @recent_weight : user.weight) - user.aim).round(2)
+    @posts          = @user.posts.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,28 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   validates :name, :started_at, :aim, :weight, :height, presence: true
+
+  def bmi
+    (weight / (height ** 2)).round(2)
+  end
+
+  def latest_bmi
+    if posts.any?
+      (posts.first.weight / (height ** 2)).round(2)
+    else
+      bmi
+    end
+  end
+
+  def latest_weight
+    if posts.any?
+      posts.first.weight
+    else
+      weight
+    end
+  end
+
+  def by_target_weight
+    ((posts.any? ? latest_weight : weight) - aim).round(2)
+  end
 end
