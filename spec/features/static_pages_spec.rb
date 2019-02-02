3dd3a7c4644eef203276d434834ee09b78ece082
Ryu007 <ryuta.tashiro@gmail.com>
2019-02-03 01:00:22 +0900

    Add spec/features/static_pages_spec.rb

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
-
 RSpec.feature "StaticPages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+  scenario "access to home as a guest" do
+    visit root_path
+    expect(page).to have_current_path root_path
+    expect(page).to have_title "100Days Of Training"
+    within '#navbarSupportedContent' do
+      expect(page).to have_content "Home"
+      expect(page).to have_content "ABOUT"
+      expect(page).to have_content "HELP"
+      expect(page).to have_content "CONTACT"
+    end
+    within '.top-page' do
+      within 'h1' do
+        expect(page).to have_content "Welcome to \"100Days of Training\"!"
+      end
+      expect(page).to have_content "Sign Up Now!"
+      expect(page).to have_content "Log In"
+    end
+  end
+
+  scenario "display about page" do
+    visit root_path
+    expect(page).to have_current_path root_path
+    within '#navbarSupportedContent > ul > li:nth-child(2)' do
+      click_link "ABOUT"
+    end
+    expect(page).to have_current_path about_path
+    find_link "@all_akumasan"
+  end
+
+  scenario "display help page" do
+    visit root_path
+    expect(page).to have_current_path root_path
+    within '#navbarSupportedContent > ul > li:nth-child(3)' do
+      click_link "HELP"
+    end
+    expect(page).to have_current_path help_path
+  end
+
+  scenario "display contact page" do
+    visit root_path
+    expect(page).to have_current_path root_path
+    within '#navbarSupportedContent > ul > li:nth-child(4)' do
+      click_link "CONTACT"
+    end
+    expect(page).to have_current_path contact_path
+    find_link "@all_akumasan"
+  end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
-
 RSpec.feature "Users", type: :feature do
+  # scenario "it creates new user" do
+  #   visit root_path
+  #   click_link "Sign Up Now!"
+  #   expect(page).to have_current_path " /users/sign_up"
+  #   expect do
+  #     fill_in "name", with: "Test User"
+  #     fill_in "email", with: "test_user@example.com"
+  #     fill_in "started_at", with: 2019-01-01
+  #     fill_in "finished_at", with: 2019-03-31
+  #     fill_in "weight", with: 88.88
+  #     fill_in "aim", with: 66.66
+  #     fill_in "height", with: 1.75
+  #
+  #     expect(page).to have_content "Welcome! You have signed up successfully."
+  #     expect(page).to have_content
+  #   end
+  # end
   pending "add some scenarios (or delete) #{__FILE__}"
 end
