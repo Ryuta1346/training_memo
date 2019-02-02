69f274a8fb40e6979d118a1ce37ce8d6952dfa62
Ryu007 <ryuta.tashiro@gmail.com>
2019-02-02 22:23:28 +0900

    Modify static_pages_controller.rb and shared/_user_top.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
-      @user  = current_user
-      @posts = current_user.posts.all
-      @post  = current_user.posts.build
+      @user  = User.find_by(id: current_user.id)
+      @posts = @user.posts.all
+      @post  = @user.posts.build
     end
   end
 
