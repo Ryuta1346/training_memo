a1bc1a71de9a68c4f04f0acfcb0c090d6f3d58ee
Ryu007 <ryuta.tashiro@gmail.com>
2019-02-15 00:00:39 +0900

    Modify Rspec

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -7,10 +7,7 @@ RSpec.feature "Posts", type: :feature do
   let!(:other_post) { create(:post, user: other_user) }
 
   before do
-    visit posts_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
+    sign_in user
   end
 
   scenario "render posts/index" do
@@ -24,7 +21,7 @@ RSpec.feature "Posts", type: :feature do
     within '.card-text' do
       expect(page).to have_content post.date
       expect(page).to have_content post.weight
-      expect(page).to have_content "#{(post.weight/ (user.height**2)).round(2)}%"
+      expect(page).to have_content "#{(post.weight / (user.height ** 2)).round(2)}%"
       expect(page).to have_content post.training
       expect(page).to have_content post.morning
       expect(page).to have_content post.lunch
@@ -33,5 +30,31 @@ RSpec.feature "Posts", type: :feature do
     end
     click_link "Return Home"
     expect(page).to have_current_path root_path
+  end
+
+  scenario "post new training" do
+    sign_in user
+    visit root_path
+    fill_in "post_weight", with: post.weight
+    # fill_in "post_image", with: "crossfit-medium.jpg"
+    fill_in "post_morning", with: post.morning
+    fill_in "post_lunch", with: post.lunch
+    fill_in "post_dinner", with: post.dinner
+    fill_in "post_training", with: post.training
+    fill_in "post_content", with: post.content
+    select "2019", from: "post_date_1i"
+    select "January", from: "post_date_2i"
+    select "1", from: "post_date_3i"
+    expect { click_button "Post" }.to change(Post, :count).by(1)
+    expect(page).to have_content "Post created!"
+    expect(page).to have_current_path root_path
+  end
+
+  scenario "User create a post with blank" do
+    sign_in user
+    visit root_path
+    click_button "Post"
+    expect(page).to have_content "Post Not created!"
+    expect(current_path).to eq root_path
   end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,25 +6,59 @@ RSpec.feature "Users", type: :feature do
   let!(:post) { create(:post, user: user) }
   let!(:other_post) { create(:post, user: other_user) }
 
-  scenario "render users/index" do
-    visit users_path
-    expect(page).to have_css '.alert-alert'
-    expect(page).to have_current_path new_user_session_path
+  scenario "login as a authenticated user" do
+    visit new_user_session_path
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Log in"
     expect(page).to have_content "Signed in successfully."
     expect(page).to have_current_path root_path
-    visit users_path
-    expect(page).to have_css ".card-users"
   end
 
-  scenario "render user page" do
-    visit new_user_session_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
+  scenario "authenticated user logged out from root_path" do
+    sign_in user
+    visit root_path
+    within '.btn-position' do
+      click_on "Log out"
+    end
+    expect(page).to have_content "Signed out successfully."
+    expect(current_path).to eq root_path
+    expect(page).to have_selector 'h1', text: "Welcome to \"100Days of Training\"!"
+  end
+
+  scenario "authenticated user change Account Name and Email" do
+    sign_in user
+    visit root_path
+    within '.btn-position' do
+      click_on 'Edit Account'
+    end
+    expect(current_path).to eq edit_user_registration_path
+    fill_in "user_name", with: "Edit User"
+    fill_in "user_email", with: "edit_user@example.com"
+    fill_in "user_current_password", with: user.password
+    click_button "Update"
+    expect(page).to have_content "Your account has been updated successfully."
+    expect(current_path).to eq root_path
+  end
+
+  scenario "access to user page as a guest" do
+    visit user_path(user.id)
+    expect(current_path).to eq new_user_session_path
+    visit users_path
+    expect(page).to have_css '.alert-alert'
+    expect(current_path).to eq new_user_session_path
+  end
+
+  scenario "render users/index" do
+    sign_in user
+    visit users_path
+    expect(page).to have_current_path "/users"
+  end
+
+  scenario "render other user's page" do
+    sign_in user
     visit user_path(other_user.id)
+    expect(page).to have_current_path user_path(other_user.id)
     expect(page).to have_css ".col-md-5.p-lg-5.mx-auto.my-5"
     expect(page).to have_css ".card"
   end
